df2cc344eb9e7626cfcb2ec785bb2136b87f342c
Abdelaziz <Abdelaziz.elantaky@Gmail.com>
2019-04-01 19:59:13 +0200

    first commit

---
+++ app/src/main/java/com/zecode/deal/AdsMenuFramgnet.java
@@ -0,0 +1,29 @@
+package com.zecode.deal;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AdsMenuFramgnet extends Fragment {
+
+
+    public AdsMenuFramgnet() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ads_menu_framgnet, container, false);
+    }
+
+}
